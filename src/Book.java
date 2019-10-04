b50d80c98af304505e2187a24616d1f2f33a72e4
sparshgupta2212 <sparsh.gupta_cs18@gla.ac.in>
2019-10-04 11:30:50 +0530

    book using string builder

--- src/Book.java
+++ src/Book.java
@@ -5,23 +5,42 @@ public class Book {
     private int yrofJoining;
     private long isbn;
     private Author[] arrayOfAuthor;
+    private int numofAuthor;
 
 
-    public Book(String bookName, int yrofJoining, long isbn, Author[] arrayOfAuthor) {
+    public Book(String bookName, int yrofJoining, long isbn, Author[] arrayOfAuthor, int numofAuthor) {
         this.bookName = bookName;
         this.yrofJoining = yrofJoining;
         this.isbn = isbn;
         this.arrayOfAuthor = arrayOfAuthor;
+        this.numofAuthor = numofAuthor;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("enter book name: ");
         String bookName = scanner.nextLine();
+        System.out.println("enter yr of publishing: ");
         int yrofJoining = Integer.parseInt(scanner.next());
+        System.out.println("enter the isbn: ");
         long isbn = Long.parseLong(scanner.next());
         Author[] authors = new Author[5];
+        for (int i = 0; i < authors.length; i++) {
+            String authorName = scanner.nextLine();
+            String genre = scanner.nextLine();
+            int numOfBook = Integer.parseInt(scanner.next());
+            boolean expert = scanner.nextBoolean();
+        }
 
         Book book = new Book();
+    }
+
+    public int getNumofAuthor() {
+        return numofAuthor;
+    }
+
+    public void setNumofAuthor(int numofAuthor) {
+        this.numofAuthor = numofAuthor;
     }
 
     public int getYrofJoining() {
